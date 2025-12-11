d6b085d1bb15173f39347209957db0f8913c078f
Ofir Mazor <66764040+OfirMazor@users.noreply.github.com>
2025-12-11 11:24:40 +0200

    Add files via upload

--- ScriptsAndTools/Utils/Configs.py
+++ ScriptsAndTools/Utils/Configs.py
@@ -26,36 +26,30 @@ class CNFG:
 
 
     # Portal variables
+    feature_service_mapping: dict[EnviType, list[str]] = {'Development': ['FabricMapDevelopment', 'InProcessMapDevelopment'],
+                                                          'Test':        ['FabricMapTest', 'InProcessMapTest'],
+                                                          'Production':  ['FabricMapProduction', 'InProcessMapProduction']}
+
     portal_servers_names: dict[EnviType, str] = {"Development": "arcgis-srv-p.mapi.co.il",
                                                  "Test":        "arcgis-srv-p.mapi.co.il",
                                                  "Production":  "bnkl3dgisprod.mapi.co.il"}
 
+    portal_mapping: dict[EnviType, str] = {"Development": f"{portal_servers_names[Environment]}/arcgis",
+                                           "Test":        f"{portal_servers_names[Environment]}/arcgis",
+                                           "Production":  f"{portal_servers_names[Environment]}/server"}
 
-    portal_mapping: dict[EnviType, str] = {"Development": "arcgis-srv-p.mapi.co.il/arcgis",
-                                           "Test":        "arcgis-srv-p.mapi.co.il/arcgis",
-                                           "Production":  "bnkl3dgisprod.mapi.co.il/server"}
 
-
-    # feature_service_mapping: dict[EnviType, list[str]] = {'Development': ['FabricMapDevelopment', 'InProcessMapDevelopment'],
-    #                                                       'Test': ['FabricMapTest', 'InProcessMapTest'],
-    #                                                       'Production': ['FabricMapProduction', 'InProcessMapProduction']}
-
-    feature_service_mapping: dict[EnviType, list[str]] = {'Development': ['Fabric_Map__BankalModDev_MNCDB2_', 'In_Process_Map_BankalModDev_MNCDB2'],
-                                                          'Test': ['FabricMapProduction', 'InProcessMapProduction'],
-                                                          'Production': ['FabricMapProduction', 'InProcessMapProduction']}
-
-    portal_url: list[str] = f'https://{portal_mapping[Environment]}/rest/services/'
+    portal_url: str = fr"https://{portal_mapping[Environment]}/rest/services/"
     FeatureServers: list[str] = feature_service_mapping[Environment]
-    ParcelFabricFeatureServer: str = fr"{portal_url}{FeatureServers[0]}/FeatureServer"
-    InProcessFeatureServer: str = fr"{portal_url}{FeatureServers[1]}/FeatureServer"
-
-
+    ParcelFabricFeatureServer: str = fr"{portal_url}NationalCadasterEditors/{FeatureServers[0]}/FeatureServer"
+    InProcessFeatureServer: str = fr"{portal_url}NationalCadasterEditors/{FeatureServers[1]}/FeatureServer"
     gis_url: str = fr"https://{portal_servers_names[Environment]}/portal/sharing/rest"
-    version_manager_url: str = fr"https://{portal_servers_names[Environment]}/arcgis/rest/services/{feature_service_mapping[Environment][0]}/VersionManagementServer"
+    version_manager_url: str = f"{portal_url}NationalCadasterEditors/{FeatureServers[0]}/VersionManagementServer"
+
 
     # CMS variables
     CMS_url_mapping: dict[EnviType, str] = {"Development": "http://192.168.134.104:7777/manage/api/Httpclientbnklpfapi/upprjstatuspf",
-                                            "Test":        "http://192.168.135.129:7777/manage/api/Httpclientbnklpfapi/upprjstatuspf",  # Same as Development
+                                            "Test":        "http://192.168.135.129:7777/manage/api/Httpclientbnklpfapi/upprjstatuspf",
                                             "Production":  "http://cmscust-app-prod:7777/manage/api/Httpclientbnklpfapi/upprjstatuspf"}
 
     CMS_url: str = CMS_url_mapping[Environment]
