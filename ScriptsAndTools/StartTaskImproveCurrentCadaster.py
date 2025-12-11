bd42523c687cf9d23591ce5d4d236d08075f31f5
Ofir Mazor <66764040+OfirMazor@users.noreply.github.com>
2025-12-11 11:24:40 +0200

    Add files via upload

--- ScriptsAndTools/StartTaskImproveCurrentCadaster.py
+++ ScriptsAndTools/StartTaskImproveCurrentCadaster.py
@@ -2,7 +2,7 @@ from os import startfile
 from Utils.Configs import CNFG
 from Utils.TypeHints import *
 from Utils.Helpers import create_shelf, get_ProcessGUID, activate_record, load_to_records, filter_to_aoi, zoom_to_aoi, \
-    set_priority, rewrite_record_data, get_aprx_name
+                          set_priority, rewrite_record_data, get_aprx_name
 from Utils.VersionManagement import open_version
 from Utils.Validations import process_in_records, features_exist, validation_set
 from Utils.Reports import compute_matching_points_report
@@ -23,8 +23,8 @@ def display_process_data(ProcessName: str) -> None:
     CurrentMap: Map = ArcGISProject("current").listMaps('מפת עריכה')[0]
     ProcessGUID: str = get_ProcessGUID(ProcessName)
     query_name: str = f'Process {ProcessName}'
-    
-    CurrentMap.addDataFromPath(fr'{CNFG.LayerFiles}ImprovementProcessGroup.lyrx')
+
+    CurrentMap.addDataFromPath(fr'{CNFG.LayerFiles}ImprovementProcessGroup_{CNFG.Environment}.lyrx')
     Group: Layer = CurrentMap.listLayers('תכנית')[0]
     Group.name = f'{ProcessName} תכנית'
     
@@ -46,11 +46,12 @@ def display_process_data(ProcessName: str) -> None:
     filter_to_aoi(ProcessName)
 
 
-def start_task_ImproveCurrentCadaster(ProcessName: str|None, Report: bool = True) -> None:
+def start_task_ImproveCurrentCadaster(Independent: bool, ProcessName: str|None, Report: bool = True) -> None:
     """
     Workflow for starting the Improve Current Cadaster task.
 
     Parameters:
+        Independent (bool): An option to start the task environment for a different process than the one suggested by the APRX file. This option may be useful when the CMS is down. Default is False.
         ProcessName (str, Optional): The name of the cadaster process returned from the APRX file name (if the workflow began from CMS)
                                      or by an independent process name given by the user.
         Report (bool): Perform the matching points report. Default is True.
@@ -59,7 +60,8 @@ def start_task_ImproveCurrentCadaster(ProcessName: str|None, Report: bool = True
 
     set_priority()
 
-    ProcessName: str = get_aprx_name() if not ProcessName else ProcessName
+    # Whether the process is executed from CMS or independent task.
+    ProcessName: str = get_aprx_name() if not Independent else ProcessName
 
     qualified: bool = validation_set('ImproveCurrentCadaster', ProcessName)
 
@@ -89,4 +91,4 @@ def start_task_ImproveCurrentCadaster(ProcessName: str|None, Report: bool = True
 
 
 if __name__ == "__main__":
-    start_task_ImproveCurrentCadaster(ProcessName= GetParameterAsText(0), Report= GetParameter(1))
+    start_task_ImproveCurrentCadaster(Independent= GetParameter(0), ProcessName= GetParameterAsText(1), Report= GetParameter(2))
