2d54d1953a6f1d6dee9f308d62cc951b13789cf6
Ofir Mazor <66764040+OfirMazor@users.noreply.github.com>
2026-02-10 10:05:25 +0200

    Add files via upload

--- ScriptsAndTools/Utils/Configs.py
+++ ScriptsAndTools/Utils/Configs.py
@@ -5,12 +5,12 @@ from Utils.TypeHints import EnviType
 class CNFG:
     """This class holds configuration variables for various project paths and settings."""
 
-    Environment: EnviType = 'Development'
+    Environment: EnviType = 'Production'
     OwnerName: str = 'PF.'
 
     # Folders
-    SDEFolder: str = fr"\\mapi_shares\MNCDB\SDE/"
-    ParcelFabricFolder: str = fr"\\mapi_shares\MNCDB\Parcel Fabric/"
+    SDEFolder: str = "<folder-with-sde's>"
+    ParcelFabricFolder: str = "<main-workspace-folder>"
     ScriptsPath: str = f'{ParcelFabricFolder}{Environment}Environment\ScriptsAndTools/'
     TasksPath: str = f'{ParcelFabricFolder}{Environment}Environment\Tasks/'
     Library: str = f'{ParcelFabricFolder}{Environment}Environment\Library/'
@@ -21,50 +21,44 @@ class CNFG:
     # Data Sources
     SDE_mapping: dict[EnviType, str] = {"Development": "Dev", "Test": "Test", "Production": "Prod"}
     SDE: str = SDE_mapping[Environment]
-    ParcelFabricDatabase: str = f"{SDEFolder}BankalMod{SDE}(pf).sde/"
-    ParcelFabricDataset: str = fr'{ParcelFabricDatabase}{OwnerName}ParcelFabricDataset/'
+    ParcelFabricDatabase: str = f"{SDEFolder}<sde_file_name>"
+    ParcelFabricDataset: str = fr'{ParcelFabricDatabase}{OwnerName}<dataset_name>/'
 
 
     # Portal variables
-    portal_servers_names: dict[EnviType, str] = {"Development": "arcgis-srv-p.mapi.co.il",
-                                                 "Test":        "arcgis-srv-p.mapi.co.il",
-                                                 "Production":  "bnkl3dgisprod.mapi.co.il"}
+    feature_service_mapping: dict[EnviType, list[str]] = {'Development': ['FabricMapDevelopment', 'InProcessMapDevelopment'],
+                                                          'Test':        ['FabricMapTest', 'InProcessMapTest'],
+                                                          'Production':  ['FabricMapProduction', 'InProcessMapProduction']}
+
+    portal_servers_names: dict[EnviType, str] = {"Development": "<portal_url_development>",
+                                                 "Test":        "portal_url_test",
+                                                 "Production":  "portal_url_production"}
+
+    portal_mapping: dict[EnviType, str] = {"Development": f"{portal_servers_names[Environment]}/<server>",
+                                           "Test":        f"{portal_servers_names[Environment]}/<server>",
+                                           "Production":  f"{portal_servers_names[Environment]}/<server>"}
 
 
-    portal_mapping: dict[EnviType, str] = {"Development": "arcgis-srv-p.mapi.co.il/arcgis",
-                                           "Test":        "arcgis-srv-p.mapi.co.il/arcgis",
-                                           "Production":  "bnkl3dgisprod.mapi.co.il/server"}
-
-
-    # feature_service_mapping: dict[EnviType, list[str]] = {'Development': ['FabricMapDevelopment', 'InProcessMapDevelopment'],
-    #                                                       'Test': ['FabricMapTest', 'InProcessMapTest'],
-    #                                                       'Production': ['FabricMapProduction', 'InProcessMapProduction']}
-
-    feature_service_mapping: dict[EnviType, list[str]] = {'Development': ['Fabric_Map__BankalModDev_MNCDB2_', 'In_Process_Map_BankalModDev_MNCDB2'],
-                                                          'Test': ['FabricMapProduction', 'InProcessMapProduction'],
-                                                          'Production': ['FabricMapProduction', 'InProcessMapProduction']}
-
-    portal_url: list[str] = f'https://{portal_mapping[Environment]}/rest/services/'
+    portal_url: str = fr"https://{portal_mapping[Environment]}/rest/services/"
     FeatureServers: list[str] = feature_service_mapping[Environment]
     ParcelFabricFeatureServer: str = fr"{portal_url}{FeatureServers[0]}/FeatureServer"
     InProcessFeatureServer: str = fr"{portal_url}{FeatureServers[1]}/FeatureServer"
-
-
     gis_url: str = fr"https://{portal_servers_names[Environment]}/portal/sharing/rest"
-    version_manager_url: str = fr"https://{portal_servers_names[Environment]}/arcgis/rest/services/{feature_service_mapping[Environment][0]}/VersionManagementServer"
+    version_manager_url: str = f"{portal_url}{FeatureServers[0]}/VersionManagementServer"
+
 
     # CMS variables
-    CMS_url_mapping: dict[EnviType, str] = {"Development": "http://192.168.134.104:7777/manage/api/Httpclientbnklpfapi/upprjstatuspf",
-                                            "Test":        "http://192.168.135.129:7777/manage/api/Httpclientbnklpfapi/upprjstatuspf",  # Same as Development
-                                            "Production":  "http://cmscust-app-prod:7777/manage/api/Httpclientbnklpfapi/upprjstatuspf"}
+    CMS_url_mapping: dict[EnviType, str] = {"Development": "<cms_api_dev>",
+                                            "Test":        "<cms_api_test>",
+                                            "Production":  "<cms_api_prod>"}
 
     CMS_url: str = CMS_url_mapping[Environment]
 
     # Others
-    default_version_guids: dict[EnviType, str] = {"Development": "{BD3F4817-9A00-41AC-B0CC-58F78DBAE0A1}",
-                                                  "Test": None,  # TODO
-                                                  "Production": "{BD3F4817-9A00-41AC-B0CC-58F78DBAE0A1}"}
+    default_version_guids: dict[EnviType, str] = {"Development": "<development_sde.default_version_guid>",
+                                                  "Test": "<test_sde.default_version_guid>"
+                                                  "Production": "production_sde.default_version_guid"}
 
     default_version_guid: str = default_version_guids[Environment]
 
-    domain: str = "@mapi.gov.il"
+    domain: str = "<organization_domain>"
